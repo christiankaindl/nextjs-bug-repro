a5a014dc1f8e7d0a75b7fd0ccdbdd12d9c583809
Christian Kaindl <christian.kaindl@hey.com>
2021-04-17 10:37:40 +0200

    Set up initial nextjs project
    Use the standard procedure from https://nextjs.org/docs

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Bug reproduction
+        </h1>
+
+        <div className={styles.grid}>
+          <Link href="/crashing-api-route">
+            <a className={styles.card}>
+              <h3>Go to API route &rarr;</h3>
+              <p>Crashes with error</p>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </div>
+  )
+}
